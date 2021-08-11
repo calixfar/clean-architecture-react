b2e491f4cf373a86e8e4276c3ef0bdb73e0da63a
Luis Santiago <luis-santiago97@hotmail.com>
2021-08-11 08:19:04 -0500

    feat: ensure Login present error if email is invalid

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -13,13 +13,16 @@ const Login: React.FC<Props> = ({ validation }: Props) => {
     isLoading: false,
     email: '',
     password: '',
-    emailError: 'Campo obligatorio',
+    emailError: '',
     passwordError: 'Campo obligatorio',
     mainError: ''
   })
 
   useEffect(() => {
-    validation.validate('email', state.email)
+    setState({
+      ...state,
+      emailError: validation.validate('email', state.email)
+    })
   }, [state.email])
 
   useEffect(() => {
